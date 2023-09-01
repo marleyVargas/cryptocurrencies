0147c63a97e726a01322f3063bc5003d014d2c0e
Marley Vargas <mvargas@gmstek.com>
2023-09-01 14:14:29 -0500

    add estructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { HomePageComponent } from './modules/home-page/home-page.component';
+import { FavouritesComponent } from './modules/favourites/favourites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FavouritesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
